a83dfa5051a29c752bec71bd53f73e595045bd53
PooWoox <41601403+PooWoox@users.noreply.github.com>
2020-01-20 17:08:10 -0300

    Changed api baseURL for backend server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://https://poowoo-devradar-backend.herokuapp.com'
 })
 
 export default api;
